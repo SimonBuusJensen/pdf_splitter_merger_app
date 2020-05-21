16816cf0b2cfa88302a9e1e9505e0fac65bc2a88
SimonBuusJensen <Bringuer6194!>
2020-05-21 18:21:23 +0200

    Method for uploading file

--- app.py
+++ app.py
@@ -1,17 +1,50 @@
-from flask import Flask, render_template, url_for
+import os
+from flask import Flask, render_template, request, flash, redirect, url_for
+from werkzeug.utils import secure_filename
+
+from utils import allowed_file
+
+UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), "files")
 
 # Create the application instance
 app = Flask(__name__, template_folder="templates", static_folder="static")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/")
+def upload_file_home():
+    return render_template("upload_file.html")
+
+@app.route("/home")
 def home():
+    print(UPLOAD_FOLDER)
     return render_template("home.html")
 
 
 @app.route("/split_pdf")
 def split_pdf():
     return render_template("split_pdf.html")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_pdf():
+    print("Request method:", request.method)
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('home.html', filename=filename))
 
 
 if __name__ == '__main__':
