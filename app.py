298a9478173c8d25366db012f451d2a7033cfd1a
SimonBuusJensen <Bringuer6194!>
2020-05-21 14:42:02 +0200

    Created a home.html and a split_pdf.html

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/split_pdf")
+def split_pdf():
+    return render_template("split_pdf.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
